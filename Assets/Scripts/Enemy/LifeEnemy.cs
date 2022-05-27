8228dcddc77397029c14d6afb747be38fe685827
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2022-05-27 20:38:59 +0300

    added ragdoll
    Добавлено:
    - падение противников при смерти

--- Assets/Scripts/Enemy/LifeEnemy.cs
+++ Assets/Scripts/Enemy/LifeEnemy.cs
@@ -1,22 +1,32 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Enemy
 {
+    [RequireComponent(typeof(Animator))]
     public class LifeEnemy : MonoBehaviour
     {
+        private Animator animator;
+        private TargetZone _targetZone;
+
+        [Space]
+        [SerializeField] private GameObject canvas;
         [SerializeField] private int maxHealth;
         private int _currentHealth;
         [SerializeField] private Image healthBar;
+
+        [SerializeField] private float _delayDeath;
         
-        private TargetZone _targetZone;
 
         private void Start()
         {
-            _currentHealth = maxHealth;
+            animator = GetComponent<Animator>();
             
             _targetZone = transform.parent.GetComponent<TargetZone>();
             _targetZone.AddEnemy(gameObject);
+            
+            _currentHealth = maxHealth;
         }
 
         public void TakeDamage()
@@ -26,12 +36,18 @@ namespace Enemy
             healthBar.fillAmount = (float)_currentHealth / maxHealth;
             
             if (_currentHealth <= 0)
-                Killed();
+                StartCoroutine(Killed());
         }
 
-        private void Killed()
+        private IEnumerator Killed()
         {
+            animator.enabled = false;
+            canvas.SetActive(false);
+            
             _targetZone.EnemyKilled(gameObject);
+
+            yield return new WaitForSeconds(_delayDeath);
+            
             Destroy(gameObject);
         }
     }
